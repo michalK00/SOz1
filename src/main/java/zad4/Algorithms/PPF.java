0880ac7eef970815156f1e26f66626ec2da815fe
MichalK <95657053+michalK00@users.noreply.github.com>
2022-05-29 21:53:19 +0200

    added last two algorithm files (not implemented)

---
+++ src/main/java/zad4/Algorithms/PPF.java
@@ -0,0 +1,15 @@
+package zad4.Algorithms;
+
+import java.util.ArrayDeque;
+
+public class PPF extends Algorithm implements AlgorithmInterface{
+
+    public PPF(int numberOfFrames, ArrayDeque<Integer>[] processesTable, ArrayDeque<Integer> generatedSequenceOfProcesses) {
+        super(numberOfFrames, processesTable, generatedSequenceOfProcesses);
+    }
+
+    @Override
+    public void simulate() {
+
+    }
+}
---
+++ src/main/java/zad4/Algorithms/ZoneModel.java
@@ -0,0 +1,15 @@
+package zad4.Algorithms;
+
+import java.util.ArrayDeque;
+
+public class ZoneModel extends Algorithm implements AlgorithmInterface{
+
+    public ZoneModel(int numberOfFrames, ArrayDeque<Integer>[] processesTable, ArrayDeque<Integer> generatedSequenceOfProcesses) {
+        super(numberOfFrames, processesTable, generatedSequenceOfProcesses);
+    }
+
+    @Override
+    public void simulate() {
+
+    }
+}
